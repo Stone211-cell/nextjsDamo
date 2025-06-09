f447d80ce288ce1096ced6efc2171c1e380bf4b5
Stone211-cell <suvapat1905@gmail.com>
2025-06-09 17:27:31 +0700

    fix: remove unused imports to fix build

--- components/Navbar/Headeravbar.tsx
+++ components/Navbar/Headeravbar.tsx
@@ -1,5 +1,3 @@
-
-
 import { ContactIconLine } from "../Contacted/ContactIcon";
 import Dropdown from "./Dropdown";
 
@@ -8,12 +6,16 @@ const Headeravbar = () => {
     <div className="flex justify-between items-center p-4 w-full overflow-x-hidden">
       <div>Logo</div>
 
-      <div className="flex flex-wrap gap-4 items-center max-w-full">
-        <span className="whitespace-nowrap">
+      <div className="flex  gap-4 items-center max-w-full">
+        <span>
           <ContactIconLine pops="hidden" sizeicon="30" />
         </span>
-        <span className="whitespace-nowrap">iconfacebook</span>
-        <span className="whitespace-nowrap">icon066888888</span>
+        <span>
+          <ContactIconLine pops="hidden" sizeicon="10" />
+        </span>
+        <span>
+          <ContactIconLine pops="hidden" sizeicon="10" />
+        </span>
 
         <div>
           <Dropdown />
