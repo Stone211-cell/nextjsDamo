1df06fec988669334631d5e8c8112a8e611a7477
Stone211-cell <suvapat1905@gmail.com>
2025-06-09 17:07:39 +0700

    fix: remove unused imports to fix build

--- components/Navbar/Headeravbar.tsx
+++ components/Navbar/Headeravbar.tsx
@@ -3,7 +3,7 @@ import Dropdown from "./Dropdown";
 
 const Headeravbar = () => {
   return (
-    <div className="flex justify-between items-center px-8 p-4">
+    <div className="flex justify-between items-center  p-4">
       <div>Logo</div>
 
       <div>
